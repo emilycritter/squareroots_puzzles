bcf88a58c1715979a5fa613a7978a97b30298fb8
emily.c.ritter <emily.c.ritter@gmail.com>
2017-09-26 20:45:13 -0500

    Initial answers to problems one and two

--- problem_one.rb
+++ problem_one.rb
@@ -2,7 +2,25 @@
 #     Print a list of location names, sorted by weekly revenue in descending order
 # ===================================================================================
 # => Read from locations.json and customers.json files.
-# => The plan attribute on a customer is the number of leafy green packs 
+# => The plan attribute on a customer is the number of leafy green packs
 #    that they receive each week. Each pack costs $5.
 # => Do not count customers with status "unsubscribed" towards revenue.
 # => Feel free to use any library you'd like.
+require 'json'
+
+locations = File.read('locations.json')
+locations = JSON.parse(locations)
+
+customers = File.read('customers.json')
+customers = JSON.parse(customers)
+
+# Do not include customers with status "unsubscribed".
+customers = customers.select{|customer| customer['status'] != 'unsubscribed'}
+
+# Calculate weekly revenue at $5 per pack.
+locations.each do |location|
+  weekly_revenue = customers.select{|customer| customer['location_id'] == location['id']}
+                            .map {|customer| customer['plan'] * 5}
+                            .reduce(:+)
+  $stdout.puts "#{location['name']}: $#{weekly_revenue}"
+end
--- problem_two.rb
+++ problem_two.rb
@@ -4,4 +4,58 @@
 # => Read from locations.json and customers.json files.
 # => The Square Roots farm is located at 630 Flushing Ave, Brooklyn NY 11211.
 # => Feel free to use any library/API you'd like.
+require 'json'
 
+begin
+  require 'google_maps_service'
+rescue LoadError
+  puts "Please install the google_map_service gem by running:"
+  puts "`gem install google_maps_service`"
+  exit
+end
+
+class GoogleMapDelivery
+  API_KEY = 'AIzaSyADf6nbq5Vo697U1u_Pw9JsDUahM3Eyz14'
+
+  def initialize
+    @gmaps = GoogleMapsService::Client.new(
+      key: API_KEY,
+      retry_timeout: 20,      # Timeout for retrying failed request
+      queries_per_second: 10  # Limit total request per second
+    )
+  end
+
+  def get_distance (origin, destination)
+    routes = @gmaps.directions(
+      origin,
+      destination,
+      mode: 'driving',
+      alternatives: false
+    )
+    routes ? routes[0][:legs][0][:distance] : {text: 'Unavailable', value: 0}
+  end
+end
+
+# Instructions include reading from the customers.json file; however,
+# no data from customers.json is used.
+customers = File.read('customers.json')
+customers = JSON.parse(customers)
+
+locations = File.read('locations.json')
+locations = JSON.parse(locations)
+square_roots_farm = '630 Flushing Ave, Brooklyn NY 11211'
+
+begin
+  # Get driving distance for each location from the farm
+  gmaps = GoogleMapDelivery.new
+  locations.each {|location| location['distance'] = gmaps.get_distance(square_roots_farm, location['address'])}
+
+  # Sort locations by driving distance from the farm (shortest to longest)
+  locations_by_distance = locations.sort_by {|location| location['distance'][:value]}
+                                   .map{|location| "#{location['name']}: #{location['distance'][:text]}"}
+  $stdout.puts locations_by_distance
+
+rescue StandardError => e
+  puts e.message
+  raise "There was an error pulling directions from the Google Maps API"
+end
