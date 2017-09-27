f7239c79cafd2e890c68e93bb1b8b08d7823ca76
emily.c.ritter <emily.c.ritter@gmail.com>
2017-09-26 20:53:26 -0500

    Add gems to README.md

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ This is a two part code puzzle for the Full Stack Software Engineer job applicat
 1) Clone this repository to your local dev environment.
 2) Complete the two parts and make sure problem_one.rb and problem_two.rb are executable and print to stdout.
 3) Zip the files and attach to the job application.
+
+## Usage
+* Ruby version (2.3.0)
+* Install the following gems
+  * `google_maps_service (0.4.2)`
+  * `json`
+* Run the two parts by running the following within the squareroots_code_puzzle directory
+  * `ruby problem_one.rb`
+  * `ruby problem_two.rb`
--- problem_two.rb
+++ problem_two.rb
@@ -5,16 +5,9 @@
 # => The Square Roots farm is located at 630 Flushing Ave, Brooklyn NY 11211.
 # => Feel free to use any library/API you'd like.
 require 'json'
+require 'google_maps_service'
 
-begin
-  require 'google_maps_service'
-rescue LoadError
-  puts "Please install the google_map_service gem by running:"
-  puts "`gem install google_maps_service`"
-  exit
-end
-
-class GoogleMapDelivery
+class GoogleMaps
   API_KEY = 'AIzaSyADf6nbq5Vo697U1u_Pw9JsDUahM3Eyz14'
 
   def initialize
@@ -36,18 +29,18 @@ class GoogleMapDelivery
   end
 end
 
-# Instructions include reading from the customers.json file; however,
-# no data from customers.json is used.
-customers = File.read('customers.json')
-customers = JSON.parse(customers)
-
-locations = File.read('locations.json')
-locations = JSON.parse(locations)
-square_roots_farm = '630 Flushing Ave, Brooklyn NY 11211'
-
 begin
+  # Instructions include reading from the customers.json file; however,
+  # no data from customers.json is used.
+  customers = File.read('customers.json')
+  customers = JSON.parse(customers)
+
+  locations = File.read('locations.json')
+  locations = JSON.parse(locations)
+  square_roots_farm = '630 Flushing Ave, Brooklyn NY 11211'
+
   # Get driving distance for each location from the farm
-  gmaps = GoogleMapDelivery.new
+  gmaps = GoogleMaps.new
   locations.each {|location| location['distance'] = gmaps.get_distance(square_roots_farm, location['address'])}
 
   # Sort locations by driving distance from the farm (shortest to longest)
@@ -57,5 +50,5 @@ begin
 
 rescue StandardError => e
   puts e.message
-  raise "There was an error pulling directions from the Google Maps API"
+  raise "There was an error pulling directions data from the Google Maps API"
 end
